9944f29da327343ce10770425fc6f622ad23eb91
manyak-yuriy <manyak.yuriy@gmail.com>
2016-09-07 23:29:06 +0300

    first commit

---
+++ TriangleRouteMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TriangleRouteMVC.Startup))]
+namespace TriangleRouteMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
